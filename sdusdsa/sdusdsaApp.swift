11fc80370d30fafae1d16736c65480ac9fff4dbc
nguyenductai <144873856+nguyenductai@users.noreply.github.com>
2024-04-12 22:11:23 +0700

    Initial Commit

---
+++ sdusdsa/sdusdsaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sdusdsaApp.swift
+//  sdusdsa
+//
+//  Created by LC Mac on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct sdusdsaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
